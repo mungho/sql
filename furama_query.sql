528933513fee8e1e27b07ced2d8565ac920abe6c
Mung Ho <mung.hokha@gmail.com>
2025-07-27 16:29:36 +0700

    case study

--- furama_query.sql
+++ furama_query.sql
@@ -92,4 +92,49 @@ WHERE lk.ten_loai_khach = 'Diamond'
 AND kh.dia_chi LIKE '%Vinh' OR kh.dia_chi LIKE '%Quảng Ngãi';
 
 -- 12. Hiển thị thông tin ma_hop_dong, ho_ten (nhân viên), ho_ten (khách hàng), so_dien_thoai (khách hàng), ten_dich_vu, so_luong_dich_vu_di_kem (được tính dựa trên việc sum so_luong ở dich_vu_di_kem), tien_dat_coc của tất cả các dịch vụ đã từng được khách hàng đặt vào 3 tháng cuối năm 2020 nhưng chưa từng được khách hàng đặt vào 6 tháng đầu năm 2021.
+SELECT hd.ma_hop_dong, nv.ho_ten, kh.ho_ten, kh.so_dien_thoai, dv.ten_dich_vu, SUM(hdct.so_luong) AS so_luong_dich_vu_di_kem, SUM(tien_dat_coc) AS tien_dat_coc FROM hop_dong hd
+LEFT JOIN khach_hang kh ON kh.ma_khach_hang = hd.ma_khach_hang
+LEFT JOIN nhan_vien nv ON nv.ma_nhan_vien = hd.ma_nhan_vien
+LEFT JOIN dich_vu dv ON dv.ma_dich_vu = hd.ma_dich_vu
+LEFT JOIN hop_dong_chi_tiet hdct ON hdct.ma_hop_dong = hd.ma_hop_dong
+LEFT JOIN dich_vu_di_kem dvdk ON dvdk.ma_dich_vu_di_kem = hdct.ma_dich_vu_di_kem
+WHERE MONTH (hd.ngay_lam_hop_dong) BETWEEN 10 AND 12 AND YEAR(hd.ngay_lam_hop_dong) = 2020
+AND NOT EXISTS (
+SELECT * FROM hop_dong hd2 
+WHERE 
+hd2.ma_dich_vu = hd.ma_dich_vu
+AND MONTH(hd2.ngay_lam_hop_dong) BETWEEN 1 AND 6 
+AND YEAR(hd2.ngay_lam_hop_dong) = 2021)
+GROUP BY hd.ma_hop_dong, nv.ho_ten, kh.ho_ten, kh.so_dien_thoai, dv.ten_dich_vu;
 
+
+-- 13. Hiển thị thông tin các Dịch vụ đi kèm được sử dụng nhiều nhất bởi các Khách hàng đã đặt phòng. (Lưu ý là có thể có nhiều dịch vụ có số lần sử dụng nhiều như nhau).
+SELECT dvdk.*, SUM(hdct.so_luong) AS so_luong FROM hop_dong_chi_tiet hdct
+JOIN dich_vu_di_kem dvdk ON dvdk.ma_dich_vu_di_kem = hdct.ma_dich_vu_di_kem
+GROUP BY dvdk.ma_dich_vu_di_kem
+HAVING so_luong = (SELECT hdct2.so_luong FROM hop_dong_chi_tiet hdct2 ORDER BY hdct2.so_luong DESC LIMIT 1);
+
+-- 14. Hiển thị thông tin tất cả các Dịch vụ đi kèm chỉ mới được sử dụng một lần duy nhất. Thông tin hiển thị bao gồm ma_hop_dong, ten_loai_dich_vu, ten_dich_vu_di_kem, so_lan_su_dung (được tính dựa trên việc count các ma_dich_vu_di_kem).
+SELECT hd.ma_hop_dong, ldv.ten_loai_dich_vu, dvdk.ten_dich_vu_di_kem, 1 AS so_lan_su_dung
+FROM hop_dong_chi_tiet hdct
+JOIN (
+    SELECT 
+        ma_dich_vu_di_kem
+    FROM hop_dong_chi_tiet
+    GROUP BY ma_dich_vu_di_kem
+    HAVING COUNT(*) = 1
+) AS only_once ON only_once.ma_dich_vu_di_kem = hdct.ma_dich_vu_di_kem
+LEFT JOIN hop_dong hd ON hd.ma_hop_dong = hdct.ma_hop_dong
+LEFT JOIN dich_vu dv ON dv.ma_dich_vu = hd.ma_dich_vu
+LEFT JOIN loai_dich_vu ldv ON ldv.ma_loai_dich_vu = dv.ma_loai_dich_vu
+LEFT JOIN dich_vu_di_kem dvdk ON dvdk.ma_dich_vu_di_kem = hdct.ma_dich_vu_di_kem;
+
+-- 15. Hiển thi thông tin của tất cả nhân viên bao gồm ma_nhan_vien, ho_ten, ten_trinh_do, ten_bo_phan, so_dien_thoai, dia_chi mới chỉ lập được tối đa 3 hợp đồng từ năm 2020 đến 2021.
+SELECT nv.ma_nhan_vien, nv.ho_ten, td.ten_trinh_do, bp.ten_bo_phan, nv.so_dien_thoai, nv.dia_chi, COUNT( hd.ma_hop_dong) AS so_luong_hop_dong FROM nhan_vien nv
+LEFT JOIN bo_phan bp ON bp.ma_bo_phan = nv.ma_bo_phan
+LEFT JOIN trinh_do td ON td.ma_trinh_do = nv.ma_trinh_do
+LEFT JOIN hop_dong hd ON hd.ma_nhan_vien = nv.ma_nhan_vien
+WHERE YEAR(hd.ngay_lam_hop_dong) BETWEEN 2020 AND 2021
+GROUP BY nv.ma_nhan_vien
+HAVING 
+COUNT( hd.ma_hop_dong) <= 3;
